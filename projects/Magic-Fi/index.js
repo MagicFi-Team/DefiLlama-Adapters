936f71fc362e708fff5b93bf636a4a8827e4871a
blockchaingale <anthonylawson0917@gmail.com>
2025-04-26 00:35:53 +1000

    Delete Magic-Fi Protocol

---
+++ projects/Magic-Fi/index.js
@@ -0,0 +1,10 @@
+Hello, Dear Defillama Team.
+Our protocol is registered on Ponzi. I requested to the Dexs.
+Anyway, currently, we don't need to list our project to defillama.
+So can you delete our protocol on defillama?
+If it can't be deleted, can you set categorize to forks from orderly?
+Our protocol name is Magic-Fi.
+Please help our team and remove it asap.
+
+Best regards,
+Magic-Fi Team.
